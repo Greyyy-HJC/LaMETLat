0628488d924e8aa2ed5af65c21b662738ca7c619
Greyyy-HJC <hejinchen17@gmail.com>
2024-08-21 15:40:27 -0400

    gsfit

--- lametlat/gsfit/ratio_fit.py
+++ lametlat/gsfit/ratio_fit.py
@@ -1,9 +1,12 @@
 import numpy as np
 import lsqfit as lsf
+import gvar as gv
 
 from lametlat.utils.log import log_count_fit
 from lametlat.gsfit.prior_setting import two_state_fit
 from lametlat.gsfit.fit_funcs import ra_re_fcn, ra_im_fcn
+from lametlat.utils.plot_settings import *
+from lametlat.utils.resampling import bs_ls_avg
 
 
 def single_ra_fit(
@@ -75,3 +78,57 @@ def single_ra_fit(
         log_count_fit()
 
     return ra_fit_res
+
+
+def plot_ra_fit_on_data(ra_re_avg, ra_im_avg, ra_fit_res, err_tsep_ls, fill_tsep_ls, Lt, id_label, err_tau_cut=1, fill_tau_cut=1):
+
+    px = id_label["px"]
+    py = id_label["py"]
+    pz = id_label["pz"]
+    b = id_label["b"]
+    z = id_label["z"]
+ 
+    def plot_part(part, ra_avg, ra_fcn, pdf_key):
+        
+        fig, ax = default_plot()
+        
+        for id, tsep in enumerate(err_tsep_ls):
+            tau_range = np.arange(err_tau_cut, tsep + 1 - err_tau_cut)
+            err_x_ls = tau_range - tsep / 2
+            err_y_ls = gv.mean(ra_avg[id, err_tau_cut:tsep + 1 - err_tau_cut])
+            err_yerr_ls = gv.sdev(ra_avg[id, err_tau_cut:tsep + 1 - err_tau_cut])
+            
+            ax.errorbar(err_x_ls, err_y_ls, err_yerr_ls, label=r'$t_{\mathrm{sep}}$' + f' = {tsep}', color=color_ls[id], **errorb)
+
+        for id, tsep in enumerate(fill_tsep_ls):
+            fit_tau = np.linspace(fill_tau_cut - 0.5, tsep - fill_tau_cut + 0.5, 100)
+            fit_t = np.ones_like(fit_tau) * tsep
+            fit_ratio = ra_fcn(fit_t, fit_tau, ra_fit_res.p, Lt)
+
+            fill_x_ls = fit_tau - tsep / 2
+            fill_y_ls = gv.mean(fit_ratio)
+            fill_yerr_ls = gv.sdev(fit_ratio)
+
+            ax.fill_between(fill_x_ls, [v.mean + v.sdev for v in fit_ratio], [v.mean - v.sdev for v in fit_ratio], color=color_ls[id], alpha=0.4)
+
+        band_x = np.arange(-6, 7)
+        band_y_ls = np.ones_like(band_x) * gv.mean(ra_fit_res.p[pdf_key])
+        band_yerr_ls = np.ones_like(band_x) * gv.sdev(ra_fit_res.p[pdf_key])
+        ax.fill_between(band_x, band_y_ls+band_yerr_ls, band_y_ls-band_yerr_ls, color=grey, alpha=0.5, label='Fit')
+        
+        ax.set_xlabel(r'$\tau - t_{\mathrm{sep}}/2$', **fs_p)
+        ax.set_ylabel(r'$\mathcal{R}$', **fs_p)
+        ax.set_ylim(auto_ylim([err_y_ls, fill_y_ls, band_y_ls], [err_yerr_ls, fill_yerr_ls, band_yerr_ls]))
+        ax.legend(ncol=2, **fs_p)
+        ax.set_title(f'PX = {px}, PY={py}, PZ={pz}, z={z}, b={b}, {part}', **fs_p)
+        plt.tight_layout()
+        
+        return ax
+
+    # Plot real part
+    ax_real = plot_part('real', ra_re_avg, ra_re_fcn, 'pdf_re')
+
+    # Plot imaginary part
+    ax_imag = plot_part('imag', ra_im_avg, ra_im_fcn, 'pdf_im')
+
+    return ax_real, ax_imag 
--- lametlat/gsfit/sum_fit.py
+++ lametlat/gsfit/sum_fit.py
@@ -1,10 +1,13 @@
 # %%
 import numpy as np
 import lsqfit as lsf
+import gvar as gv
 
 from lametlat.utils.log import log_count_fit
 from lametlat.gsfit.fit_funcs import sum_re_fcn, sum_im_fcn
 from lametlat.gsfit.prior_setting import summation_fit
+from lametlat.utils.resampling import bs_ls_avg
+from lametlat.utils.plot_settings import *
 
 
 def single_sum_fit(sum_re_avg, sum_im_avg, tsep_ls, tau_cut, id_label):
@@ -55,3 +58,36 @@ def single_sum_fit(sum_re_avg, sum_im_avg, tsep_ls, tau_cut, id_label):
         log_count_fit()
 
     return sum_fit_res
+
+def plot_sum_fit_on_data(sum_re_avg, sum_im_avg, sum_fit_res, err_tsep_ls, fill_tsep_ls, id_label, tau_cut):
+    
+    px = id_label["px"]
+    py = id_label["py"]
+    pz = id_label["pz"]
+    b = id_label["b"]
+    z = id_label["z"]
+
+    def plot_part(part, sum_avg, sum_fcn):
+        
+        fig, ax = default_plot()
+        
+        ax.errorbar(err_tsep_ls, gv.mean(sum_avg), yerr=gv.sdev(sum_avg), label='Data', color=blue, **errorb)
+        
+        fit_t = np.linspace(fill_tsep_ls[0]-0.5, fill_tsep_ls[-1]+0.5, 100)
+        fit_sum = sum_fcn(fit_t, tau_cut, sum_fit_res.p)
+        
+        ax.fill_between(fit_t, [v.mean + v.sdev for v in fit_sum], [v.mean - v.sdev for v in fit_sum], label='Fit', color=blue, alpha=0.4)
+        
+        ax.set_xlabel(r'$t_{\mathrm{sep}}$', **fs_p)
+        ax.set_ylabel(r'$\mathcal{S}$', **fs_p)
+        ax.legend(**fs_p)
+        ax.set_title(f'PX = {px}, PY={py}, PZ={pz}, z={z}, b={b}, {part}', **fs_p)
+        ax.set_ylim(auto_ylim([gv.mean(sum_avg), gv.mean(fit_sum)], [gv.sdev(sum_avg), gv.sdev(fit_sum)]))
+        plt.tight_layout()
+        
+        return ax
+        
+    ax_real = plot_part('real', sum_re_avg, sum_re_fcn)
+    ax_imag = plot_part('imag', sum_im_avg, sum_im_fcn)
+
+    return ax_real, ax_imag
