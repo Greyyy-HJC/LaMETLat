c7578806327108ef361f32b4d18f786ed5bbb37e
Greyyy-HJC <hejinchen17@gmail.com>
2024-09-26 13:53:44 -0400

    FT_correct

--- lametlat/utils/fourier_transform.py
+++ lametlat/utils/fourier_transform.py
@@ -1,101 +1,8 @@
 """
-FT and inverse FT, with chosen convention.
+FT and inverse FT, using the convention in Peskin.
 """
 
 import numpy as np
-
-'''
-@jit(nopython=True)
-def sum_ft(x_ls, fx_ls, delta_x, output_k):
-    """FT: f(x) -> f(k), coordinate to momentum by discrete sum, produce complex numbers
-    the f(x) cannot be gvar list, because of the complex calculation
-
-    Args:
-        x_ls (list): x list of f(x)
-        fx_ls (list): y list of f(x)
-        delta_x (float): the gap between two values in x_ls
-        output_k (float): the k value to output f(k)
-
-    Returns:
-        complex: f(k)
-    """
-    x_arr = np.asarray(x_ls)
-    fx_arr = np.asarray(fx_ls)
-    return delta_x / (2 * np.pi) * np.sum(np.exp(1j * x_arr * output_k) * fx_arr)
-
-
-@jit(nopython=True)
-def sum_ft_re_im(x_ls, fx_re_ls, fx_im_ls, delta_x, output_k):
-    """FT: f(x) -> f(k), coordinate to momentum by discrete sum, produce real and imaginary part separately
-    the f(x) can be gvar list
-
-    Args:
-        x_ls (list): x list of f(x)
-        fx_re_ls (list): y list of the real part of f(x)
-        fx_im_ls (list): y list of the imaginary part of f(x)
-        delta_x (float): the gap between two values in x_ls
-        output_k (float): the k value to output f(k)
-
-    Returns:
-        tuple: f(k) real and imaginary part separately
-    """
-    x_arr = np.asarray(x_ls)
-    fx_re_arr = np.asarray(fx_re_ls)
-    fx_im_arr = np.asarray(fx_im_ls)
-    
-    cos_term = np.cos(x_arr * output_k)
-    sin_term = np.sin(x_arr * output_k)
-    
-    val_re = delta_x / (2 * np.pi) * (np.sum(cos_term * fx_re_arr) - np.sum(sin_term * fx_im_arr))
-    val_im = delta_x / (2 * np.pi) * (np.sum(sin_term * fx_re_arr) + np.sum(cos_term * fx_im_arr))
-
-    return val_re, val_im
-
-
-@jit(nopython=True)
-def sum_ft_inv(k_ls, fk_ls, delta_k, output_x):
-    """Inverse FT: f(k) -> f(x), momentum to coordinate by discrete sum, produce complex numbers
-
-    Args:
-        k_ls (list): k list of f(k)
-        fk_ls (list): y list of f(k)
-        delta_k (float): the gap between two values in k_ls
-        output_x (float): the x value to output f(x)
-
-    Returns:
-        complex: f(x)
-    """
-    k_arr = np.asarray(k_ls)
-    fk_arr = np.asarray(fk_ls)
-    return delta_k * np.sum(np.exp(-1j * k_arr * output_x) * fk_arr)
-
-
-@jit(nopython=True)
-def sum_ft_inv_re_im(k_ls, fk_re_ls, fk_im_ls, delta_k, output_x):
-    """Inverse FT: f(k) -> f(x), momentum to coordinate by discrete sum, produce real and imaginary part separately
-
-    Args:
-        k_ls (list): k list of f(k)
-        fk_re_ls (list): y list of the real part of f(k)
-        fk_im_ls (list): y list of the imaginary part of f(k)
-        delta_k (float): the gap between two values in k_ls
-        output_x (float): the x value to output f(x)
-
-    Returns:
-        tuple: f(x) real and imaginary part separately
-    """
-    k_arr = np.asarray(k_ls)
-    fk_re_arr = np.asarray(fk_re_ls)
-    fk_im_arr = np.asarray(fk_im_ls)
-    
-    cos_term = np.cos(k_arr * output_x)
-    sin_term = np.sin(k_arr * output_x)
-    
-    val_re = delta_k * (np.sum(cos_term * fk_re_arr) - np.sum(sin_term * fk_im_arr))
-    val_im = delta_k * (np.sum(sin_term * fk_re_arr) + np.sum(cos_term * fk_im_arr))
-
-    return val_re, val_im
-'''
 
 # * Without numba is even faster
 
@@ -147,7 +54,7 @@ def sum_ft_re_im(x_ls, fx_re_ls, fx_im_ls, delta_x, output_k):
     return val_re, val_im
 
 
-def sum_ft_inv(k_ls, fk_ls, delta_k, output_x):
+def sum_inv_ft(k_ls, fk_ls, delta_k, output_x):
     """Inverse FT: f(k) -> f(x), momentum to coordinate by discrete sum, produce complex numbers
 
     Args:
@@ -166,7 +73,7 @@ def sum_ft_inv(k_ls, fk_ls, delta_k, output_x):
     return val
 
 
-def sum_ft_inv_re_im(k_ls, fk_re_ls, fk_im_ls, delta_k, output_x):
+def sum_inv_ft_re_im(k_ls, fk_re_ls, fk_im_ls, delta_k, output_x):
     """Inverse FT: f(k) -> f(x), momentum to coordinate by discrete sum, produce real and imaginary part separately
 
     Args:
@@ -182,11 +89,11 @@ def sum_ft_inv_re_im(k_ls, fk_re_ls, fk_im_ls, delta_k, output_x):
     k_ls = np.array(k_ls)
     fk_re_ls = np.array(fk_re_ls)
     fk_im_ls = np.array(fk_im_ls)
-    val_re = delta_k * np.sum(np.cos(k_ls * output_x) * fk_re_ls) - delta_k * np.sum(
+    val_re = delta_k * np.sum(np.cos(k_ls * output_x) * fk_re_ls) + delta_k * np.sum(
         np.sin(k_ls * output_x) * fk_im_ls
     )
-    val_im = delta_k * np.sum(np.sin(k_ls * output_x) * fk_re_ls) + delta_k * np.sum(
-        np.cos(k_ls * output_x) * fk_im_ls
+    val_im = delta_k * np.sum(np.cos(k_ls * output_x) * fk_im_ls) - delta_k * np.sum(
+        np.sin(k_ls * output_x) * fk_re_ls
     )
 
     return val_re, val_im
@@ -215,7 +122,7 @@ if __name__ == "__main__":
 
     # Perform Inverse Fourier Transform
     delta_k = k[1] - k[0]
-    fx_reconstructed = np.array([sum_ft_inv(k, fk, delta_k, xi).real for xi in x])
+    fx_reconstructed = np.array([sum_inv_ft(k, fk, delta_k, xi).real for xi in x])
 
     # Plotting
 
