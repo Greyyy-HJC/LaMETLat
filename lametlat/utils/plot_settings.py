8550e496cdb7bc2a0ddc34df78a3cf6e6480b8e9
Greyyy-HJC <hejinchen17@gmail.com>
2024-10-07 21:08:31 -0400

    sub plot

--- lametlat/utils/plot_settings.py
+++ lametlat/utils/plot_settings.py
@@ -21,14 +21,40 @@ violet = "#7C5AB8"
 fuschia = "#C3559F"
 
 color_ls = [
-    blue, orange, green, red, violet, fuschia,
-    turquoise, grape, lime, peach, sunkist, yellow,
+    blue,
+    orange,
+    green,
+    red,
+    violet,
+    fuschia,
+    turquoise,
+    grape,
+    lime,
+    peach,
+    sunkist,
+    yellow,
 ]
 
 # Marker settings
 marker_ls = [
-    ".", "o", "s", "P", "X", "*", "p", "D",
-    "<", ">", "^", "v", "1", "2", "3", "4", "+", "x",
+    ".",
+    "o",
+    "s",
+    "P",
+    "X",
+    "*",
+    "p",
+    "D",
+    "<",
+    ">",
+    "^",
+    "v",
+    "1",
+    "2",
+    "3",
+    "4",
+    "+",
+    "x",
 ]
 
 # Font settings
@@ -39,6 +65,7 @@ font_config = {
 }
 
 from matplotlib import rcParams
+
 rcParams.update(font_config)
 
 # Figure size settings
@@ -64,10 +91,11 @@ errorb = {
 tmin_label = r"$t_{\mathrm{min}}~/~a$"
 tmax_label = r"$t_{\mathrm{max}}~/~a$"
 tau_center_label = r"$(\tau - t_{\rm{sep}}/2)~/~a$"
-tsep_label = r'${t_{\mathrm{sep}}~/~a}$'
-z_label = r'${z~/~a}$'
+tsep_label = r"${t_{\mathrm{sep}}~/~a}$"
+z_label = r"${z~/~a}$"
 lambda_label = r"$\lambda = z P^z$"
-meff_label = r'${m}_{\mathrm{eff}}~/~\mathrm{GeV}$'
+meff_label = r"${m}_{\mathrm{eff}}~/~\mathrm{GeV}$"
+
 
 def auto_ylim(y_data, yerr_data, y_range_ratio=4):
     """
@@ -81,15 +109,20 @@ def auto_ylim(y_data, yerr_data, y_range_ratio=4):
     Returns:
         tuple: y-axis limits (y_min, y_max).
     """
-    all_y = np.concatenate([y + yerr for y, yerr in zip(y_data, yerr_data)] +
-                           [y - yerr for y, yerr in zip(y_data, yerr_data)])
+    all_y = np.concatenate(
+        [y + yerr for y, yerr in zip(y_data, yerr_data)]
+        + [y - yerr for y, yerr in zip(y_data, yerr_data)]
+    )
     y_min, y_max = np.min(all_y), np.max(all_y)
     y_range = y_max - y_min
-    return y_min - y_range/y_range_ratio, y_max + y_range/y_range_ratio
+    return y_min - y_range / y_range_ratio, y_max + y_range / y_range_ratio
+
 
 def default_plot():
     """
     Create a default plot.
+    
+    Usage: fig, ax = default_plot()
 
     Returns:
         fig: matplotlib.figure.Figure
@@ -100,6 +133,36 @@ def default_plot():
     ax.tick_params(direction="in", top="on", right="on", **ls_p)
     ax.grid(linestyle=":")
     return fig, ax
+
+
+def default_sub_plot(height_ratio=3):
+    """
+    Create a plot with two subplots, with a specified height ratio.
+    
+    Usage: fig, (ax1, ax2) = default_sub_plot()
+
+    Args:
+        height_ratio (int): The ratio of heights between the upper and lower subplots. Default is 3.
+
+    Returns:
+        fig: matplotlib.figure.Figure
+        (ax1, ax2): tuple of matplotlib.axes.Axes
+    """
+    fig, (ax1, ax2) = plt.subplots(
+        2,
+        1,
+        figsize=fig_size,
+        gridspec_kw={"height_ratios": [height_ratio, 1]},
+        sharex=True,
+    )
+    fig.subplots_adjust(hspace=0)
+
+    for ax in (ax1, ax2):
+        ax.tick_params(direction="in", top="on", right="on", **ls_p)
+        ax.grid(linestyle=":")
+
+    return fig, (ax1, ax2)
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
