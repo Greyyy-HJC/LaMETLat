cd3bff255857d0523b8af090c0f15c1c7719a8fd
Greyyy-HJC <hejinchen17@gmail.com>
2025-05-20 13:18:55 -0400

    update read h5

--- lametlat/preprocess/print_h5.py
+++ lametlat/preprocess/print_h5.py
@@ -1,5 +1,6 @@
 from typing import Any, Callable
 import h5py
+import sys
 
 
 def print_h5(group: h5py.Group, level=0, logger: Callable[[Any], Any] = print):
@@ -18,3 +19,21 @@ def print_h5(group: h5py.Group, level=0, logger: Callable[[Any], Any] = print):
             logger(level * "\t" + key + ":")
             subgroup = group[key]
             print_h5(subgroup, level + 1)
+
+# %%
+if __name__=='__main__':
+    if len(sys.argv)==1 or \
+        (len(sys.argv)==2 and (sys.argv[1]=='-h' or sys.argv[1]=='--help')):
+        print("""usage: \
+to print the structure of HDF5 files.
+\tshell>> python print_h5.py file1 [file2 ...]""")
+        exit(0)
+
+    for file in sys.argv[1:]:
+        try:
+            print(file+':')
+            print_h5(h5py.File(file,'r'),1)
+            pass
+        except Exception:
+            print('\tERROR')
+            pass
